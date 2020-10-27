f79bed5db477250ffbf89c4f57f427afc9f677ea
Jorge Sanchez <jorge.a.sanchezII@outlook.com>
2020-10-27 11:57:53 -0400

    Created new project enabling authentication/authorization. Added to the new form but getting weird error.

---
+++ Data/Models/VehicleType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarM8_01.Data.Models
+{
+    public class VehicleType
+    {
+        public string ID { get; set; }
+        public string Type { get; set; }
+
+        public static List<VehicleType> Types = new List<VehicleType>()
+        {
+            new VehicleType() { ID= "Convertible", Type = "Convertible"},
+            new VehicleType() { ID= "Coupe", Type = "Coupe"},
+            new VehicleType() { ID= "Hatchback", Type = "Hatchback"},
+            new VehicleType() { ID= "Minivan/Van", Type = "Minivan/Van"},
+            new VehicleType() { ID= "Sedan", Type = "Sedan"},
+            new VehicleType() { ID= "Station Wagon", Type = "Station Wagon"},
+            new VehicleType() { ID= "SUV", Type = "SUV"},
+            new VehicleType() { ID= "Truck", Type = "Truck"},
+        };
+    }
+}
---
+++ Data/VehiclesService.cs
@@ -0,0 +1,80 @@
+﻿using CarM8_01.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarM8_01.Data
+{
+    public class VehiclesService
+    {
+        private readonly Carm8dataContext _context;
+
+        public VehiclesService(Carm8dataContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Vehicles> GetVehicles()
+        {
+            return _context.Vehicles.AsNoTracking();
+        }
+
+        public async Task<Vehicles> GetVehiclesAsync(int VehicleID)
+        { // Get the existing vehicle
+          var ExistingVehicle = await _context.Vehicles
+              .Where(x => x.VehicleId == VehicleID) 
+              .AsNoTracking() 
+              .FirstOrDefaultAsync(); 
+          
+          return ExistingVehicle;
+        }
+
+        public Task<Vehicles> CreateVehicleAsync(Vehicles newVehicles)
+        {
+            try
+            {
+                // Add a new help desk ticket.
+                _context.Vehicles.Add(newVehicles);
+                _context.SaveChanges();
+
+                return Task.FromResult(newVehicles);
+            }
+            catch (Exception ex)
+            {
+                DetachAllEntities(); 
+                throw;
+            }
+        }
+
+        public void DetachAllEntities()
+        { // When we have an error, we need
+          // to remove EF Core change tracking.
+          var changedEntriesCopy = _context.ChangeTracker.Entries() 
+              .Where(e => e.State == EntityState.Added || 
+                          e.State == EntityState.Modified || 
+                          e.State == EntityState.Deleted)
+              .ToList(); 
+          foreach (var entry in changedEntriesCopy) 
+              entry.State = EntityState.Detached;
+        }
+    }
+}
+//return Task.FromResult(index => new Vehicles
+//{
+//Year = index.Year,
+//Make = index.Make,
+//Model = index.Model,
+//Color = index.Color
+//}).ToArray<Vehicles>();
+
+//var rng = new Random();
+//return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+//{
+//    Date = startDate.AddDays(index),
+//    TemperatureC = rng.Next(-20, 55),
+//    Summary = Summaries[rng.Next(Summaries.Length)]
+//}).ToArray());
